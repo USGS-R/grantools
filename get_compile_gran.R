0b8864712c147eb62852c6ebfa404f09096d3949
Luke Winslow <lwinslow@usgs.gov>
2015-02-04 16:25:48 -0600

    Trying to make compile code multi-platform

--- get_compile_gran.R
+++ get_compile_gran.R
@@ -4,13 +4,22 @@ library(devtools)
 library(tools)
 
 r_maj_min = substr(paste0(R.Version()$major, '.', R.Version()$minor), 0, 3)
+os = Sys.info()['sysname']
 
-gran_dir = 'GRAN'
+if(os == 'Windows'){
+	build_ext = '.zip'
+}else if(os == 'Darwin'){
+	build_ext = '.tgz'
+}else{
+	stop('unrecognized OS type', os)
+}
+
+gran_dir = '../GRAN'
 src_dir = file.path(gran_dir, 'src', 'contrib')
 build_dir = file.path(gran_dir, 'bin', 'windows', 'contrib', r_maj_min)
 
 
-gran <- c(GRAN="http://owi.usgs.gov")
+gran <- c(GRAN="http://owi.usgs.gov/R")
 gran_packages = available.packages(contriburl = contrib.url(gran, type='source'))
 
 makeRepo(gran_packages, path=gran_dir, repos=gran, type="source")
@@ -22,7 +31,7 @@ dir.create(build_dir, recursive=TRUE)
 for(i in 1:nrow(gran_packages)){
 	
 	package = paste0(gran_packages[i,'Package'], '_', gran_packages[i,'Version'], '.tar.gz')
-	binary = paste0(gran_packages[i,'Package'], '_', gran_packages[i,'Version'], '.zip')
+	binary = paste0(gran_packages[i,'Package'], '_', gran_packages[i,'Version'], build_ext)
 	package_path = file.path(src_dir, package)
 	binary_path = file.path('.', binary)
 	binary_dest = file.path(build_dir, binary)
@@ -34,9 +43,9 @@ for(i in 1:nrow(gran_packages)){
 	file.rename(binary_path, binary_dest)
 }
 
-#Once done, write PACKAGES file
-write_PACKAGES(build_dir, type='win.binary')
+#Once done, write PACKAGES file, use default pkgType for this platform (mac/win)
+write_PACKAGES(build_dir, type=getOption('pkgType'))
 
 ## sync with GRAN
-
+#email luke
 
