fa9b58e40630f461ff402712f32aa9cdd44888fb
Jordan S Read <jread@usgs.gov>
2015-04-27 16:24:49 -0500

    updating powstreams

--- dl_build_src_gran.R
+++ dl_build_src_gran.R
@@ -28,6 +28,9 @@ dir.create(src_dir, recursive = TRUE)
 scratch = tempdir()
 
 for(i in 1:nrow(packages)){
+	
+	cat(packages$package[i])
+	
 	url = paste0('http://github.com/', packages$package[i], '/archive/', packages$tag[i], '.zip')
 
 	GET(url, write_disk(file.path(scratch, 'package.zip'), overwrite=TRUE))
@@ -36,7 +39,7 @@ for(i in 1:nrow(packages)){
 	
 	pkgdirname = Sys.glob(paste0(scratch, '/', packages$package[i], '/', basename(packages$package[i]), '*'))
 	
-	install_deps(pkgdirname)
+	devtools::install_deps(pkgdirname)
 	
 	if(length(pkgdirname) > 1){
 		stop('too many files in downloaded zip, ambiguous build info')
