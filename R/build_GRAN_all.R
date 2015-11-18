1c435c4691ee58439ce585d8ed6c809e0d5951fb
Jordan S Read <jread@usgs.gov>
2015-11-17 19:32:02 -0600

    adding DL to the build_all function

--- R/build_GRAN_all.R
+++ R/build_GRAN_all.R
@@ -5,7 +5,7 @@ build_GRAN_all <- function(versions){
 	if (os == 'Darwin'){
 		for (version in versions){
 			system(sprintf('ln -sfhv /Library/Frameworks/R.framework/Versions/%s /Library/Frameworks/R.framework/Versions/Current', version))
-			system("Rscript -e 'library(granbuild);dl_build_bin()'")
+			system("Rscript -e 'library(granbuild);dl_build_src();dl_build_bin()'")
 		}
 	} else {
 		message(os, 'not configured for version switching')
