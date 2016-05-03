b04f5567faf19e53c91cbfefb1e06c200a8bddbc
Luke Winslow <lwinslow@usgs.gov>
2016-05-03 13:57:09 -0500

    working out new HTML generating for GRAN

---
+++ R/generate_gran_html.R
@@ -0,0 +1,28 @@
+#' @title Generate GRAN package index and html sites
+#' 
+#' @description 
+#' There are many ways to do this:
+#' 1. \url{http://watson.nci.nih.gov/bioc_mirror/packages/release/bioc/vignettes/biocViews/inst/doc/createReposHtml.pdf}|
+#' 2. https://github.com/hadley/staticdocs
+#' 3. Rd2HTML (maybe?)
+#' 4. install.packages('biocViews', repos = c('http://bioconductor.org/packages/release/bioc', 'http://cran.rstudio.com'))
+#' 
+#' 
+#' @import biocViews
+#' 
+#' @export
+generate_gran_html = function(GRAN.dir='./GRAN'){
+	
+	contribPaths <- c(source="src/contrib",
+		win64.binary="bin/windows/contrib/3.2",
+		mac.binary.mavericks="bin/macosx/mavericks/contrib/3.2")
+	
+	extractVignettes(GRAN.dir, contribPaths["source"])
+	extractVignettes(GRAN.dir, contribPaths["source"])
+	genReposControlFiles(GRAN.dir, contribPaths)
+	
+	writeRepositoryHtml(GRAN.dir, title='GRAN')
+	
+	
+}
+
--- demo/parse_gran_s3_logs.R
+++ demo/parse_gran_s3_logs.R
@@ -11,20 +11,31 @@ logfiles = Sys.glob(paste0(log_dir, '/*'))
 
 all_logs = data.frame()
 
-for(i in 1:length(logfiles)){
-	tryCatch({
-		tmp = read.table(logfiles[i], sep=' ', stringsAsFactors=FALSE)
-		all_logs = rbind(all_logs, tmp)
-	}, error = function(e){})
-	
-}
+all_dfs = lapply(logfiles, function(fname){
+							tmp = data.frame()
+							tryCatch({
+								tmp = read.table(fname, sep=' ', stringsAsFactors=FALSE)
+							}, error = function(e){})
+							return(tmp)
+})
 
+all_cols = lapply(all_dfs, ncol)
+all_dfs = all_dfs[all_cols == 19]
+
+all_logs = as.data.frame(data.table::rbindlist(all_dfs, fill=TRUE))
+
+all_logs = na.omit(all_logs[,paste0('V', 1:19)])
 
 names(all_logs) = c('owner', 'bucket', 'time', 'time2', 'ip', 'requester', 'requestid', 
 										'operation', 'key', 'requesturi', 'httpstatus', 'errorcode', 
 										'bytessent', 'objsize', 'totaltime', 'turnaroundtime', 'referrer',
 										'user-agent', 'versionid')
 
+
+
+link_ips = data.frame(ip = unique(all_logs$ip))
+
+link_ips = geocode.ips(link_ips$ip)
 
 
 #all_logs$country = geocode.ips.country(all_logs$ip)$country
@@ -46,7 +57,7 @@ filter_packages = function(df){
 }
 
 just_packages = filter_packages(all_logs)
-just_packages$country = geocode.ips.country(just_packages$ip)$country
+#just_packages$country = geocode.ips.country(just_packages$ip)$country
 	
 extract_package_names = function(keys){
 	
@@ -65,4 +76,6 @@ package_summary = group_by(just_packages, packagename) %>%
 
 write.csv(package_summary, '~/gran_package_dl_stats.csv', row.names=FALSE)
 
-
+beepr::beep(sound=2)
+beepr::beep(sound=2)
+beepr::beep(sound=2)
