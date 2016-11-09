55e33ad875ff908218cb050094cb733b90e11be7
Laura DeCicco <ldecicco@usgs.gov>
2016-11-09 15:56:40 -0600

    Function-ify.

--- R/check_num_packages.R
+++ R/check_num_packages.R
@@ -1,11 +1,30 @@
-#check that number of packages on the repo matches gran_source_list.tsv
-
-
-srcList <- read.table(system.file("gran_source_list.tsv",package = "granbuild"),
-                      stringsAsFactors = FALSE, header = TRUE)
-
-onGRAN <- available.packages(contriburl = contrib.url('https://owi.usgs.gov/R'))
-
-if(nrow(onGRAN) != nrow(srcList)){
-  error("Number of packages available on GRAN does not match the source list")
+#' checkNumbers
+#' 
+#' check that number of packages on the repo matches gran_source_list.tsv
+#' 
+#' @export
+checkNumbers <- function(){
+  srcList <- read.table(system.file("gran_source_list.tsv",package = "granbuild"),
+                        stringsAsFactors = FALSE, header = TRUE)
+  
+  onGRAN <- available.packages(contriburl = contrib.url('https://owi.usgs.gov/R'))
+  
+  if(nrow(onGRAN) != nrow(srcList)){
+    error("Number of packages available on GRAN does not match the source list")
+  }
+  
+  onGRAN.win <- readLines('https://owi.usgs.gov/R/bin/windows/contrib/3.3/PACKAGES')
+  onGRAN.win <- length(grep(pattern = "Package: ", onGRAN.win, fixed = TRUE))
+  
+  if(onGRAN.win != nrow(srcList)){
+    error("Number of Windows 3.3 packages available on GRAN does not match the source list")
+  }
+  
+  onGRAN.win <- readLines('https://owi.usgs.gov/R/bin/windows/contrib/3.2/PACKAGES')
+  onGRAN.win <- length(grep(pattern = "Package: ", onGRAN.win, fixed = TRUE))
+  
+  if(onGRAN.win != nrow(srcList)){
+    error("Number of Windows 3.2 packages available on GRAN does not match the source list")
+  }
+  
 }
