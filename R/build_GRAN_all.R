135d10dd0a4a1761a20ffa491356cbb8d4235720
wdwatkins <wwatkins@usgs.gov>
2017-01-25 17:20:40 -0600

    only build binaries on 3.2

--- R/build_GRAN_all.R
+++ R/build_GRAN_all.R
@@ -4,7 +4,7 @@
 #' each of the versions specified with the \code{versions} parameter.
 #' 
 #' @param versions character vector of R major.minor versions (e.g., '3.2').
-#' 
+#' @importFrom utils compareVersion
 #' @examples 
 #' \dontrun{
 #' build_GRAN_all(c('3.2','3.3'))
@@ -15,9 +15,16 @@ build_GRAN_all <- function(versions){
 	os = Sys.info()['sysname']
 	if (os == 'Darwin'){
 	  orig.version = dir_version()
+	  if(length(versions) > 1 && compareVersion(versions[1], versions[2]) == -1){
+	    stop("Currently only building source for 3.3, so 3.3 needs to build first")
+	  } 
 	  for (version in versions){
 		  set_version(version)
-			system("Rscript -e 'library(granbuild); dl_build_src(); build_bin()'")
+	    if(version == "3.2") {
+	      system("Rscript -e 'library(granbuild); build_bin()'")
+	    } else {
+	      system("Rscript -e 'library(granbuild); dl_build_src(); build_bin()'")
+	    }
 		}
 	  #reset version 
 	  set_version(orig.version)
