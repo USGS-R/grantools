49a362e52a8193a1c517ef75c3a61727e5e79db3
wdwatkins <wwatkins@usgs.gov>
2017-10-03 15:13:32 -0500

    build out shell script to do the version switching since it segfaults from Rstudio

--- inst/mac_build.sh
+++ inst/mac_build.sh
@@ -3,8 +3,30 @@
 #do everything build_GRAN_all does for the mac build, then sync w/AWS
 #run this from the main package directory, not from ./inst!
 
-Rscript -e 'library(granbuild); build_GRAN_all(c('3.4', '3.3'))'
+pointer=/Library/Frameworks/R.framework/Versions/Current
+versions=(3.4 3.3)
+origVersion=$(readlink $pointer)
+for ver in ${versions[@]}
+do
+	ln -sfhv $ver $pointer #change version
 
+	#have to be a directory up - is there a better alternative here?
+	cd ~/Documents/R
+	R CMD INSTALL --no-multiarch --with-keep.source grantools	
+	cd ~/Documents/R/grantools
+
+	#only build source for first version
+	if [ $ver == ${versions[0]} ]
+	then
+		Rscript -e 'library(granbuild); dl_build_src(); build_bin()'
+	else
+		Rscript -e 'library(granbuild); build_bin()'
+	fi
+done
+
+ln -sfhv $origVersion $pointer  #reset to original version
+
+#push to S3
 aws s3 sync ~/Documents/R/grantools/GRAN/bin/macosx/mavericks/contrib s3://owi-usgs-gov/R/bin/macosx/mavericks/contrib --delete --profile chsprod
 aws s3 sync ~/Documents/R/grantools/GRAN/bin/macosx/el-capitan/contrib s3://owi-usgs-gov/R/bin/macosx/el-capitan/contrib --delete --profile chsprod
 
